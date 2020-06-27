420f45ccf158f3e53b0e46fecab7f6417276f205
Jie.Huang <929661205@qq.com>
2020-06-27 14:19:05 +0800

    46 dfs

---
+++ 46.js
@@ -0,0 +1,38 @@
+// 给定一个 没有重复 数字的序列，返回其所有可能的全排列。
+
+// 示例:
+
+// 输入: [1,2,3]
+// 输出:
+// [
+//   [1,2,3],
+//   [1,3,2],
+//   [2,1,3],
+//   [2,3,1],
+//   [3,1,2],
+//   [3,2,1]
+// ]
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function (nums) {
+  let result = [];
+  dfs([]);
+  function dfs(path) {
+    if (path.length === nums.length) {
+      result.push([...path]);
+      return;
+    }
+    for (let i = 0; i < nums.length; i++) {
+      if (path.includes(nums[i])) {
+        continue;
+      }
+      path.push(nums[i]);
+      dfs(path);
+      path.pop();
+    }
+  }
+  return result;
+};
