f08c45a4f9436718d476b995e4094ae7027a5b86
Jie.Huang <929661205@qq.com>
2020-07-17 16:16:11 +0800

    55 solved

--- 55.js
+++ 55.js
@@ -19,6 +19,16 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var canJump = function(nums) {
-
+var canJump = function (nums) {
+  let farthest = 0;
+  if (nums.length === 1) {
+    return true;
+  }
+  for (let i = 0; i < nums.length && i <= farthest; i++) {
+    farthest = Math.max(farthest, i + nums[i]);
+    if (farthest >= nums.length - 1) {
+      return true;
+    }
+  }
+  return false;
 };
